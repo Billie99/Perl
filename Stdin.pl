62b262b03beb8594de34df189785996779251d38
Billie99 <billiearmstrong1886@gmail.com>
2017-01-30 00:55:19 +0100

    Aggiunto nuovo file

---
+++ Stdin.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+# funzione <STDIN>
+my $nome;
+
+print "Inserisci il tuo nome : ";
+$nome = <STDIN>;
+
+my $cognome;
+
+print "Inserisci il tuo cognome : ";
+$cognome = <STDIN>;
+
+print "Il tuo nome e' " , $nome;
+print "Il tuo cognome e' " , $cognome;
