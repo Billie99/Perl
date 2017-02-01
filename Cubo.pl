cc46fe9961844b8ff7c55d21e8cda9bd4a3ae66e
Billie99 <billiearmstrong1886@gmail.com>
2017-02-01 01:39:54 +0100

    Aggiunto nuovo file

---
+++ Cubo.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+print "Calcolare il volume di un cubo \n";
+my $lato;
+
+print "Inserisci il valore del lato ";
+$lato = <STDIN>;
+
+$volume = $lato * $lato * $lato;
+print "Il volume del cubo e': $volume \n";
+
+
