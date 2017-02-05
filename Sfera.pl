21df3077f57c7f778cb3cd20862bd5d998e8b9f6
Billie99 <billiearmstrong1886@gmail.com>
2017-02-05 17:17:13 +0100

    Aggiustamenti

--- Sfera.pl
+++ Sfera.pl
@@ -9,9 +9,9 @@ print "Inserisci il valore del raggio \n";
 $Raggio = <STDIN>;
 
 my $Superficie = (4 * $pi) * ($Raggio * $Raggio);
-print "Il valore della superficie e': $Superficie \n";
+print "Il valore della superficie e': " .($Superficie). "\n";
 
 my $Volume = (4.0/3.0) * $pi * ($Raggio * $Raggio * $Raggio);
-print "Il volume della sfera e': $Volume \n";
+print "Il volume della sfera e': " .($Volume). "\n";
 
 
